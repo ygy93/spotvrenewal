0eea0a2f12c14abdb7b94e805e70d949887f5fa6
didrmsdud <dev.voalffl93@gmail.com>
2022-07-30 05:11:02 +0900

    loginfinish

--- js/script.js
+++ js/script.js
@@ -29,10 +29,20 @@ $(".btnx").click(function(){
 });
 
 
-// 비밀번호 눈동자이미지 클릭 시 이미지 변환
+// 비밀번호 눈동자이미지 클릭 시 이미지 변환 ( ----------미완성---------- )
 $(".form .pw img").click(function(){
     $(".form .pw img:nth-of-type(2)").toggleClass("on");
 });
+
+$(".form-info .pw-info img").click(function(){
+    $(".form-info .pw-info img:nth-of-type(2)").toggleClass("on");
+});
+
+$(".form-info .pw-check img").click(function(){
+    $(".form-info .pw-check img:nth-of-type(2)").toggleClass("on");
+});
+
+
 
 
 // 아이디, 비밀번호칸 정보입력 시 버튼 활성화 ( ----------미완성---------- )
@@ -46,10 +56,41 @@ $("input").on("input", function(){
 });
 
 
+
+
+
+
 // 로그인 실패창 닫기
 $(".btnf").click(function(){
    $("#fail").css({"visibility":"hidden"});
 });
+
+
+// 아이디 찾기 실패창 닫기
+$(".btnid").click(function(){
+   $("#popid").css({"visibility":"hidden"});
+});
+
+
+// 비밀번호 찾기 실패창 닫기
+$(".btnpw").click(function(){
+   $("#poppw").css({"visibility":"hidden"});
+});
+
+
+// 모든 체크박스 미체크 시 생성되는 팝업창 닫기
+$(".btnck").click(function(){
+   $("#check").css({"visibility":"hidden"});
+});
+
+
+// 이메일 인증을 하지 않고 버튼 클릭 시 생성되는 팝업창 닫기
+$(".btncert").click(function(){
+   $("#certf").css({"visibility":"hidden"});
+});
+
+
+
 
 
 // 가입 시 입력했던 이메일을 적으면 버튼 활성화 ( ----------미완성---------- )
@@ -67,6 +108,14 @@ $(".id-sc input").click(function(){
     $(this).toggleClass("on");
 });
 
+$(".form-cert form > div input").click(function(){
+    $(this).toggleClass("on");
+});
+
+$(".form-info form > div input").click(function(){
+    $(this).toggleClass("on");
+});
+
 
 
 $("input").on("input", function(){
@@ -81,6 +130,42 @@ $("input").on("input", function(){
 $(".pw-sc input").click(function(){
     $(this).toggleClass("on");
 });
+
+
+
+
+// 회원가입 첫번째 페이지 체크박스 동의
+var check = false;
+
+// 체크박스 전체 선택
+$("#agcheck").on("click", function(){
+	if($(this).is(":checked")){
+		// 전체동의를 체크했을 경우 개별동의도 체크
+		$(".terms").prop("checked", true);
+	}else{
+		// 전체동의를 체크하지 않았을 경우 개별동의도 체크해제
+		$(".terms").prop("checked", false);
+	}
+});
+
+
+// 체크박스 개별 선택
+$(".form-in form > div").on("click", ".terms", function() {
+    var is_checked = true;
+
+    $(".form-in form > div .terms").each(function(){
+        is_checked = is_checked && $(this).is(":checked");
+    });
+
+    $("#agcheck").prop("checked", is_checked);
+});
+
+
+
+
+// 인증번호 발송 버튼 클릭 시 발송 메세지 나타내기 ( ----------미완성---------- )
+
+
 
 
 
