66499cbb2dacec82f97543ca1db3d883d3f748da
didrmsdud <dev.voalffl93@gmail.com>
2022-07-29 17:52:18 +0900

    3

--- js/script.js
+++ js/script.js
@@ -29,3 +29,63 @@ $(".btnx").click(function(){
 });
 
 
+// 비밀번호 눈동자이미지 클릭 시 이미지 변환
+$(".form .pw img").click(function(){
+    $(".form .pw img:nth-of-type(2)").toggleClass("on");
+});
+
+
+// 아이디, 비밀번호칸 정보입력 시 버튼 활성화 ( ----------미완성---------- )
+$("input").on("input", function(){
+    var user_id = $(".user_id").val();
+    var user_pw = $(".user_pw").val();
+
+    if ( user_id == "" || user_pw == "" ){
+        $(".loginbtn input").css({"opacity":"1"});
+    }
+});
+
+
+// 로그인 실패창 닫기
+$(".btnf").click(function(){
+   $("#fail").css({"visibility":"hidden"});
+});
+
+
+// 가입 시 입력했던 이메일을 적으면 버튼 활성화 ( ----------미완성---------- )
+$("input").on("input", function(){
+    var user_email = $(".user_email").val();
+
+    if ( user_email == ""){
+        $(".idsc-btn input").css({"opacity":"1"});
+    }
+});
+
+
+// 이메일을 적으면 이메일란 바닥 선 색상 활성화 ( ----------미완성---------- )
+$(".id-sc input").click(function(){
+    $(this).toggleClass("on");
+});
+
+
+
+$("input").on("input", function(){
+    var user_email = $(".user_email").val();
+
+    if ( user_email == ""){
+        $(".pwsc-btn input").css({"opacity":"1"});
+    }
+});
+
+
+$(".pw-sc input").click(function(){
+    $(this).toggleClass("on");
+});
+
+
+
+
+
+
+
+
